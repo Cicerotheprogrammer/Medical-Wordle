4ba0edcfa59cf0063ffe5a275cf9d4e45e4cfeb5
Matthew Bucy <66077798+Ranger347@users.noreply.github.com>
2022-02-04 08:28:54 -0500

    Update controls.js

--- server/controllers/controls.js
+++ server/controllers/controls.js
@@ -1,11 +1,11 @@
 export const getHomePage = (req, res) => {
     //Put the links for a specific route for each class here
-    res.sendFile("C:/Users/catsr/Documents/Computer Science/Medical-Wordle/server/index/health.html");
+    res.sendFile();
 }
 
 export const getHealth = (req, res) => {
     //Send the file for the correct wordlist
-    res.send();
+    res.sendFile("C:/Users/catsr/Documents/Computer Science/Medical-Wordle/server/index/health.html");
 }
 
 export const getCivics = (req, res) => {
