88c71ff03d8a5b3cf94c8c46d52e5bfd34387bf6
Patrick Hultberg <90772733+Cicerotheprogrammer@users.noreply.github.com>
2022-02-04 08:18:11 -0500

    a

--- server/controllers/controls.js
+++ server/controllers/controls.js
@@ -1,6 +1,10 @@
 export const getHomePage = (req, res) => {
     //Put the links for a specific route for each class here
+<<<<<<< Updated upstream
     res.sendFile("C:/Users/catsr/Documents/Computer Science/Medical-Wordle/server/html/health.html");
+=======
+    
+>>>>>>> Stashed changes
 }
 
 export const getHealth = (req, res) => {
