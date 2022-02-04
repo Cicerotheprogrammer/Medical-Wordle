ba5ad16c88d55a1db530dd0e54a3fba0feef0997
Matthew Bucy <66077798+Ranger347@users.noreply.github.com>
2022-02-03 20:47:03 -0500

    node

--- server/controllers/controls.js
+++ server/controllers/controls.js
@@ -1,7 +1,6 @@
-//not needed. ie take out when done.
 export const getHomePage = (req, res) => {
     //Put the links for a specific route for each class here
-    
+    res.sendFile("C:/Users/catsr/Documents/Computer Science/Medical-Wordle/server/html/health.html");
 }
 
 export const getHealth = (req, res) => {
