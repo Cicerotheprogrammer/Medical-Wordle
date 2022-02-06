28013a155ad035234d42bc93bf36a3463ed42525
Matthew Bucy <66077798+Ranger347@users.noreply.github.com>
2022-02-06 14:04:18 -0500

    chose random word

--- server/index/code.js
+++ server/index/code.js
@@ -1,3 +1,18 @@
 import BODYSTRUCTURESANDFUNCTIONS from "./wordlists/body_structures_and_functions.js";
 import DIVERSIFIEDHEALTHOCCUPATIONS from "./wordlists/diversified_health_occupations.js";
 
+const pickRandomTerm = () => {
+    var chosenWord;
+    //Pick one of the two books
+    let book = Math.floor(Math.random() * 2);
+    if (book == 0) {
+        chosenWord = BODYSTRUCTURESANDFUNCTIONS[Math.floor(Math.random() * BODYSTRUCTURESANDFUNCTIONS.length-1)];
+    }   else {
+        chosenWord = DIVERSIFIEDHEALTHOCCUPATIONS[Math.floor(Math.random() * DIVERSIFIEDHEALTHOCCUPATIONS.length-1)];
+    }
+
+    return chosenWord.toUpperCase();
+
+}
+
+console.log(pickRandomTerm());
